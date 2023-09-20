41fc17de5679b737f69cae56f0f01e9efe9f5736
TomLBZ <libozhao142857@hotmail.com>
2023-09-20 13:24:58 +0800

    updated dockerfile

--- DollMaker/run.sh
+++ DollMaker/run.sh
@@ -5,5 +5,18 @@
 # this is used to name the docker container
 cname=$(pwd | xargs basename)
 cnamelower=$(echo $cname | tr '[:upper:]' '[:lower:]')
+
+# check if a docker container with the same name is already running
+if [ "$(docker ps -q -f name=$cnamelower)" ]; then
+    # stop the docker container
+    docker stop $cnamelower
+fi
+
+# check if a docker with the same name exists
+if [ "$(docker ps -aq -f status=exited -f name=$cnamelower)" ]; then
+    # remove the docker container
+    docker rm $cnamelower
+fi
+
 # run the docker container
 docker run -d --name $cnamelower $cnamelower
